7e971c3fb6715a546d4e68754f098276b39a22ce
wei shan <xy.520.yu@gmail.com>
2022-11-02 10:45:10 +0100

    added intent.addFlags

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -27,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void signup(View view){
         Intent intent=new Intent(this,SignupActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
         startActivity(intent);
+
     }
 }
