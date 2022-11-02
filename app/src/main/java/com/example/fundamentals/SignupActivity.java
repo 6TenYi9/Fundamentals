4154d9f0022f4514c61aa10ef8de2076041cade8
wei shan <xy.520.yu@gmail.com>
2022-11-02 10:36:26 +0100

    Added return option to cancel

--- app/src/main/java/com/example/fundamentals/SignupActivity.java
+++ app/src/main/java/com/example/fundamentals/SignupActivity.java
@@ -2,13 +2,31 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
+
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        cancel=findViewById(R.id.cancel);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goback(view);
+            }
+        });
+    }
+    public void goback (View view){
+        Intent intent=new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
