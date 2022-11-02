8702a7187eb06640264c25378131533b78f289a5
wei shan <xy.520.yu@gmail.com>
2022-11-02 10:29:23 +0100

    sign up activity done

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        go=findViewById(R.id.ok);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup(view);
+            }
+        });
+    }
+    public void signup(View view){
+        Intent intent=new Intent(this,SignupActivity.class);
+        startActivity(intent);
     }
 }
