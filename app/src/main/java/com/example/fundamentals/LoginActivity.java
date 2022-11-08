8f676933efe6646cbc3144ab714e21b3f413fc11
wei shan <xy.520.yu@gmail.com>
2022-11-08 14:07:43 +0100

    Start First  on Fundamentals

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button go;
+    ImageView mGirl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,13 @@ public class LoginActivity extends AppCompatActivity {
                 signup(view);
             }
         });
+
+        mGirl=findViewById(R.id.girl);
+
+        /*Glide.with(this)
+                .load(R.drawable.girl)
+                .into(mGirl);*/
+
     }
     public void signup(View view){
         Intent intent=new Intent(this,SignupActivity.class);
