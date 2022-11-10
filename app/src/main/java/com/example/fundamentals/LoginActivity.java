825c43b070b36767634c3ecc1935198d0b3a9206
wei shan <xy.520.yu@gmail.com>
2022-11-10 09:59:25 +0100

    change logo by user image

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button go;
@@ -29,9 +31,15 @@ public class LoginActivity extends AppCompatActivity {
 
         mGirl=findViewById(R.id.girl);
 
-        /*Glide.with(this)
-                .load(R.drawable.girl)
-                .into(mGirl);*/
+        Glide.with(this)
+                //.load(R.drawable.girl)
+                .load("https://images.unsplash.com/photo-1640343631072-37ff06fd3397?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80") //persone
+                //.load("https://i.pinimg.com/236x/60/2a/7a/602a7a466b24603a7f7fc69588e84cb3--iphone--wallpaper-phone-wallpapers.jpg")
+                //.load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                //.load("https://wallpaperaccess.com/full/1796653.jpg") //dragon
+                //.centerCrop()
+                .circleCrop()
+                .into(mGirl);
 
     }
     public void signup(View view){
